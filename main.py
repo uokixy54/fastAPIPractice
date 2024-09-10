0824a1a473fdffb39013675cdffee469f9e3ff32
uokixy54 <uokixy.lemberry@gmail.com>
2024-09-11 01:20:43 +0900

    テストコミット

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "https://uokixy54.github.io/ProjectStaticQuiz-/"  # GitHub PagesのURL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/sample")
 def read_root():
